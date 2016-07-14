299c977cc9166206e11f8d21df1087ff9c004982
Andries Smit <mr.andries.smit@gmail.com>
2016-07-14 13:27:57 +0200

    Code cleaning and new functions.
    - Upgrade to latest Dropzone Lib (v4.3.0  AMD)
    - Add Auto upload option
    - Make use of libs parallel upload processing.
    - Use latest boilerplate Mendix.
    - Added Grunt files.
    - Added addition debug loggin and error feedback.
    - Improved Mendix Test project
    - Enabled debug in index.html
    - Updated latest to XSD
    - Moved CSS into ui/widgetname.css
    - Remove images (not used in latest lib).

--- src/DropZone/widget/DropZone.js
+++ src/DropZone/widget/DropZone.js
@@ -1,5 +1,27 @@
 /*jslint browser: true, devel:true, nomen:true, unparam:true, regexp: true*/
-/*global define, require, mxui, mx, Dropzone, logger*/
+/*global define, require, mxui, mx, mendix, Dropzone, logger*/
+
+/*
+    DropZone
+    ========================
+    @file      : Dropzone.js
+    @version   : 3.1.0
+    @author    : Andries Smit & Chris de Gelder
+    @date      : 14-07-2016
+    @license   : Apache V2
+
+    Documentation
+    ========================
+    Drop multiple images or documents and upload
+
+    
+    To be done:
+    - fix, upload button image
+    - is relative dimention, with, height.
+    - add modeler parameters for caption of the remove, cancel button and cancel quesiton.
+    - test fallback, scenario
+
+ */
 
 define([
     "dojo/_base/declare",
@@ -7,7 +29,7 @@ define([
     "dojo/dom-construct",
     "dojo/_base/lang",
     "DropZone/widget/lib/dropzone"
-], function (declare, _WidgetBase, domConstruct, _TemplatedMixin, dojoArray, dojoLang, _Dropzone) {
+], function (declare, _WidgetBase, domConstruct, dojoLang, Dropzone) {
     "use strict";
 
     // Declare widget's prototype.
@@ -19,134 +41,132 @@ define([
         panelheight: 200,
         panelwidth: 500,
         buttoncaption: "upload",
-        contextGUID: null,
         uploadButton: null,
-        myDropZone: null,
-        parallelCount: 0,
-
+        dropzone: null,
+        parallelUploads: 4,
+        _contextObj: null,
+        
+        // dojo.declare.constructor is called to construct the widget instance. Implement to initialize non-primitive properties.
+        constructor: function () {
+            logger.debug(this.id + ".constructor");
+            this.dropzone = null;
+            this._contextObj = null;
+        },        
+        // dijit._WidgetBase.postCreate is called after constructing the widget. Implement to do extra setup work.
         postCreate: function () {
             logger.debug(this.id + ".postCreate");
             this.initDropZone();
-            this.actLoaded();
         },
 
-        applyContext: function (context, callback) {
-            logger.debug(this.id + ".applyContext");
-            if (context) {
-                this.contextGUID = context.getTrackId();
-            }
-            if (callback) {
-                callback();
-            }
+        // mxui.widget._WidgetBase.update is called when context is changed or initialized. Implement to re-render and / or fetch data.
+        update: function (obj, callback) {
+            logger.debug(this.id + ".update");
+            this._contextObj = obj;
+            mendix.lang.nullExec(callback);
         },
-
+        // initalize the dropzone library. 
         initDropZone: function () {
             logger.debug(this.id + ".initDropZone");
             domConstruct.empty(this.domNode);
-            this.uploadButton = new mxui.widget._Button({
-                caption: this.buttoncaption,
-                onClick: dojoLang.hitch(this, this.onclickEvent),
-                icon: "mxclientsystem/mxui/widget/styles/images/MxFileInput/uploading.gif"
-            });
-            this.domNode.appendChild(this.uploadButton.domNode);
-            //this.uploadButton.hideIcon();
-            this.domNode.appendChild(mxui.dom.div({
-                id: this.id + "_zone",
-                "class": "dropzone",
-                style: "height: " + this.panelheight + "px; width: " + this.panelwidth + "px; background-color: gray;"
-            }));
-            Dropzone.autoDiscover = false;
-            Dropzone.maxFilesize = this.maxFileSize;
-            this.myDropzone = new Dropzone("#" + this.id + "_zone", {
-                url: "/file?fileID=4&maxFileSize=5", // dummy file id
-                paramName: "mxdocument",
-                autoProcessQueue: false,
-                addRemoveLinks: true,
-                accept: dojoLang.hitch(this, this.accept)
-            });
-            if (this.removecaption) {
-                this.myDropzone.options.dictRemoveFile = this.removecaption;
+            if (!this.autoUpload) {
+                this.uploadButton = new mxui.widget._Button({
+                    caption: this.buttoncaption,
+                    onClick: dojoLang.hitch(this, this.onclickEvent),
+                    icon: "mxclientsystem/mxui/widget/styles/images/MxFileInput/uploading.gif"
+                });
+                this.domNode.appendChild(this.uploadButton.domNode);
             }
-            this.myDropzone.on("complete", dojoLang.hitch(this, this.onComplete));
-            this.myDropzone.on("error", dojoLang.hitch(this, this.onError));
-            this.myDropzone.on("removedfile", dojoLang.hitch(this, this.onRemoveFile));
+            this.domNode.appendChild(mxui.dom.div({
+                "id": this.id + "_zone",
+                "class": "dropzone",
+                "style": "height: " + this.panelheight + "px; width: " + this.panelwidth + "px;"
+            }));
+            this.dropzone = new Dropzone("#" + this.id + "_zone", {
+                autoDiscover: false,
+                maxFilesize: this.maxFileSize,
+                url: dojoLang.hitch(this, this.getMendixURL),
+                paramName: "mxdocument",
+                autoProcessQueue: this.autoUpload,
+                addRemoveLinks: true,
+                dictDefaultMessage: this.message,
+                accept: dojoLang.hitch(this, this.accept),
+                parallelUploads: this.parallelUploads
+            });
+            this.dropzone.on("success", dojoLang.hitch(this, this.onComplete));
+            this.dropzone.on("error", dojoLang.hitch(this, this.onError));
+            this.dropzone.on("removedfile", dojoLang.hitch(this, this.onRemoveFile));
         },
-
+        // set the Mendix upload URL based on the GUID
+        getMendixURL: function (files) {
+            logger.debug(this.id + ".getMendixURL");
+            return "/file?guid=" + files[0].obj.getGuid() + "&maxFileSize=" + this.maxFileSize + "&csrfToken=" + mx.session.getCSRFToken() + "&height=75&width=100";
+        },
+        // on error remove the files.
         onError: function (file, message) {
-            logger.debug(this.id + ".onError");
-            this.removeFile(file);
-            this.parallelCount += 1;
-            this.sendNextFile();
-        },
-
+            logger.error(this.id + ".onError", message);
+            this.onRemoveFile(file);
+        },        
+        // an image should be removed from within a microflow, if there is non just delete if via the api
         onRemoveFile: function (file, message) {
-            logger.debug(this.id + ".onRemoveFile");
-            var obj = file.obj,
-                mf = this.onRemove,
-                args;
-            if (obj && mf !== "") {
-                args = {
+            if (this._beingDestroyed) {
+                // dont remove the files when the widget is being destroyed by the uninitialize function.
+                return;
+            }
+            logger.debug(this.id + ".onRemoveFile");            
+            var obj = file.obj;
+            if (obj && this.onRemove) {
+                mx.data.action({
                     params: {
-                        actionname: mf,
+                        actionname: this.onRemove,
                         applyto: "selection",
                         guids: [obj.getGuid()]
                     },
+                    store: {
+                        caller: this.mxform
+                    },
                     callback: dojoLang.hitch(this, function (result) {
                         file.obj = null;
-                        // image should be removed in microflow.
                     }),
                     error: function (e) {
                         logger.error("onRemoveFile", e);
                     }
-                };
-                mx.data.action(args);
+                });
             } else {
                 this.removeFile(file);
             }
         },
-
+        // when uploadload is completed, commit and call onchange MF
         onComplete: function (file, message) {
             logger.debug(this.id + ".onComplete");
             if (file.obj) {
                 mx.data.commit({
                     mxobj: file.obj,
-                    callback: function () {
+                    callback: dojoLang.hitch(this, function () {
                         logger.debug("onComplete");
-                    }
+                        this.callOnChange(file.obj);
+                    })
                 });
-                this.parallelCount += 1;
-                this.sendNextFile();
             }
         },
-
+        // Create file on mendix server, and validate if it could be accepted.
         accept: function (file, callback) {
-            logger.debug(this.id + ".accept");
-            mx.data.create({
-                "entity": this.imageentity,
-                "callback": dojoLang.hitch(this, this.addedFile, file, callback)
-            });
+            this.createMendixFile(file, dojoLang.hitch(this, function () {
+                this.acceptMendix(file, callback);
+            }));
         },
-
-        addedFile: function (file, callback, obj) {
-            logger.debug(this.id + ".addedFile");
-            var mf = this.onAccept, rejectcaption = "reject", args;
-            obj.set(this.nameattr, file.name);
-            if (this.sizeattr) {
-                obj.set(this.sizeattr, file.size);
-            }
-            if (this.typeattr) {
-                obj.set(this.typeattr, file.type);
-            }
-            file.obj = obj;
-            if (file && mf !== "") {
-                if (this.rejectcaption) {
-                    rejectcaption = this.rejectcaption;
-                }
-                args = {
+        // Validate if object will be accepted by the mendix server
+        acceptMendix: function (file, callback) {
+            logger.debug(this.id + ".accept");
+            var rejectcaption = this.rejectcaption || "rejected";
+            if (file.obj && this.onAccept) {
+                mx.data.action({
                     params: {
-                        actionname: mf,
+                        actionname: this.onAccept,
                         applyto: "selection",
-                        guids: [obj.getGuid()]
+                        guids: [file.obj.getGuid()]
+                    },
+                    store: {
+                        caller: this.mxform
                     },
                     callback: dojoLang.hitch(this, function (result) {
                         if (!result) {
@@ -158,13 +178,38 @@ define([
                     error: function (e) {
                         logger.error("addedFile", e);
                     }
-                };
-                mx.data.action(args);
+                });
             } else {
                 callback();
             }
+        },        
+        // create a mendix file on the server when new upload item is added.
+        createMendixFile: function (file, callback) {
+            logger.debug(this.id + ".createMendixFile");
+            mx.data.create({
+                entity: this.imageentity,
+                callback: dojoLang.hitch(this, function (obj) {
+                    var ref = this.contextassociation.split("/");
+                    if (obj.has(ref[0])) {
+                        obj.set(ref[0], this._contextObj.getGuid());
+                    }
+                    obj.set(this.nameattr, file.name);
+                    if (this.sizeattr) {
+                        obj.set(this.sizeattr, file.size);
+                    }
+                    if (this.typeattr) {
+                        obj.set(this.typeattr, file.type);
+                    }
+                    file.obj = obj;
+                    callback();
+                }),
+                error: function () {
+                    logger.error("failed createMendixFile");
+                    callback();
+                }
+            });
         },
-
+        // remove file directly via the client API.
         removeFile: function (file) {
             logger.debug(this.id + ".removeFile");
             if (file.obj) {
@@ -179,93 +224,39 @@ define([
                 });
             }
         },
-
-        onclickEvent: function (e) {
+        // on click of the upload button start processing the upload queue
+        onclickEvent: function () {
             logger.debug(this.id + ".onclickEvent");
-            this.parallelCount = 4;
-            this.sendNextFile();
+            this.dropzone.processQueue();
         },
-
-        sendNextFile: function () {
-            logger.debug(this.id + ".sendNextFile");
-            if (this.myDropzone.getQueuedFiles().length > 0) {
-                var file = this.myDropzone.getQueuedFiles().shift();
-                this.myDropzone.options.url = "/file?guid=" + file.obj.getGuid() + "&maxFileSize=" + this.maxFileSize + "&csrfToken=" + mx.session.getCSRFToken() + "&height=75&width=100";
-                this.callOnChange(file, file.obj);
-            }
-        },
-
-        callOnChange: function (file, obj) {
+        // call onchange Miroflow if any. 
+        callOnChange: function (obj) {
             logger.debug(this.id + ".callOnChange");
-            if (obj) {
-                var ref = this.contextassociation.split("/"),
-                    mf = this.onChangemf,
-                    args;
-                if (obj.has(ref[0])) {
-                    obj.set(ref[0], this.contextGUID);
-                }
-                if (mf !== "") {
-                    args = {
-                        params: {
-                            actionname: mf,
-                            applyto: "selection",
-                            guids: [obj.getGuid()]
-                        },
-                        callback: dojoLang.hitch(this, function (result) {
-                            this.process(file);
-                        }),
-                        error: function (e) {
-                            logger.error("callOnChange", e);
-                        }
-                    };
-                    mx.data.action(args);
-                } else {
-                    this.process(file);
-                }
-            } else {
-                this.process(file);
+            if (obj && this.onChangemf) {
+                mx.data.action({
+                    params: {
+                        actionname: this.onChangemf,
+                        applyto: "selection",
+                        guids: [obj.getGuid()]
+                    },
+                    store: {
+                        caller: this.mxform
+                    },
+                    callback: dojoLang.hitch(this, function () {
+                        logger.debug("callOnChange");
+                    }),
+                    error: function (e) {
+                        logger.error("callOnChange", e);
+                    }
+                });
             }
         },
-
-        process: function (file) {
-            logger.debug(this.id + ".process");
-            this.myDropzone.processFile(file);
-            this.parallelCount -= 1;
-            this.waitFor(
-                function () {
-                    return this.parallelCount > 0;
-                },
-                function () {
-                    this.sendNextFile();
-                }
-            );
-        },
-
-        waitFor: function (condition, callback) {
-            logger.debug(this.id + ".waitFor");
-            function waiter(condition, callback) {
-                return function () {
-                    var condMet = false;
-                    try {
-                        condMet = condition();
-                    } catch (e) {
-                        logger.error("waitFor", e);
-                    }
-
-                    if (condMet) {
-                        callback();
-                    } else {
-                        setTimeout(waiter(condition, callback), 50);
-                    }
-                };
-            }
-            waiter(condition, callback)();
-        },
-
+        // mxui.widget._WidgetBase.uninitialize is called when the widget is destroyed. Implement to do special tear-down work.
+        // Clean up listeners, helper objects, etc. There is no need to remove listeners added with this.connect / this.subscribe / this.own.
         uninitialize: function () {
             logger.debug(this.id + ".uninitialize");
-            if (this.myDropZone) {
-                this.myDropZone.destroy();
+            if (this.dropzone) {
+                this.dropzone.destroy();
             }
         }
     });
